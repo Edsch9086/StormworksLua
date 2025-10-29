8b99304772be41e995d2a901d0a04b08525c08a3
Edsch9086 <edschaulat@gmail.com>
2025-10-29 14:07:34 -0700

    Fix clamp function implementation

--- SwerveController/swerveReader.lua
+++ SwerveController/swerveReader.lua
@@ -1,6 +1,5 @@
 function math.clamp(val, lower, upper)
-    if lower > upper then lower, upper = upper, lower end
-    return math.max(lower, math.min(upper, val))
+    return math.min(math.max(val, min), max)
 end
 
 buffer = 0
