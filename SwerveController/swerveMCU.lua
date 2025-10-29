bb950a88e7ce8fd92e4ffbce9fe039fdfd46bd08
Edsch9086 <edschaulat@gmail.com>
2025-10-29 10:08:19 -0700

    Implement world-centric control and gyro reset
    Added functionality for world-centric control and gyro reset.

--- SwerveController/swerveMCU.lua
+++ SwerveController/swerveMCU.lua
@@ -3,7 +3,6 @@ local width = property.getNumber("Chassis Width")
 local driveMultiplier = property.getNumber("Drive Multiplier")
 local steeringMultiplier = property.getNumber("Steering Multiplier")
 
-
 function onTick()
     local yDir = input.getNumber(1) * driveMultiplier
     local xDir = input.getNumber(2) * driveMultiplier
@@ -13,19 +12,20 @@ function onTick()
 	-- local worldY = input.getNumber(6)
 	-- local roll = input.getNumber(7)
 	-- local pitch = input.getnumber(8)
+	-- local rotSpeed = input.getNumber(9)
+	-- local linSpeed = input.getNumber(10)
     local modeCoast = property.getBool("Coast Mode")
     local worldCentric = property.getBool("World/Robot Centric")
     local resetGyro = property.getBool(1)
 
-
-    if worldCentric = true then -- world/field centric control
-		
+    if worldCentric == true then -- world/field centric control
+		xVel, yVel = orient(xDir, yDir, worldYaw, resetGyro)
     else 
         xVel = xDir
         yVel = yDir
     end
 	
-    local A = xVel - (rotation*length)/2
+    local A = xVel - (rotation*length)/2 -- define wheel vector locations
     local B = xVel + (rotation*length)/2
     local C = yVel - (rotation*width)/2
     local D = yVel + (rotation*width)/2
@@ -59,8 +59,20 @@ function calculateWheel(X, Y)
 	return speed, angle
 end
 
-function orient(xDir, yDir, resetGyro, compass)
-
+local gyroOffset = 0
+function orient(xDir, yDir, resetGyro, worldYaw)
+    if resetGyro then
+        gyroOffset = worldYaw
+    end
+	
+    local zeroedYaw = worldYaw - gyroOffset
+    if zeroedYaw > 0.5 then zeroedYaw = zeroedYaw - 1 end
+    if zeroedYaw < -0.5 then zeroedYaw = zeroedYaw + 1 end
+	
+    local yaw_rad = zeroedYaw * math.pi
+    local xVel = xDir * math.cos(yaw_rad) - yDir * math.sin(yaw_rad)
+    local yVel = xDir * math.sin(yaw_rad) + yDir * math.cos(yaw_rad)
+    return xVel, yVel
 end
 
 --[[ old speed/angle ()
@@ -72,6 +84,7 @@ function angle(X, Y)
     return math.atan(X,Y)*180/math.pi
 end
 ]]
+
 
 
 
