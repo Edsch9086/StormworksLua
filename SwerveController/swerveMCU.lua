8a3b79f93daef88934352865049c09ad76034e6e
Edsch9086 <edschaulat@gmail.com>
2025-10-30 11:47:31 -0700

    rewrite and relocate comments to improve readability

--- SwerveController/swerveMCU.lua
+++ SwerveController/swerveMCU.lua
@@ -1,4 +1,4 @@
-local length = property.getNumber("Chassis Length")
+local length = property.getNumber("Chassis Length") -- static vars defined in controller properties
 local width = property.getNumber("Chassis Width")
 local driveMultiplier = property.getNumber("Drive Multiplier")
 local steeringMultiplier = property.getNumber("Steering Multiplier")
@@ -6,53 +6,64 @@ local modeCoast = property.getBool("Coast Mode")
 local worldCentric = property.getBool("World/Robot Centric")
 
 function onTick()
-    local yDir = input.getNumber(1) * driveMultiplier -- control inputs
+	-- control inputs
+    local yDir = input.getNumber(1) * driveMultiplier 
     local xDir = input.getNumber(2) * driveMultiplier
     local rotation = input.getNumber(3) * steeringMultiplier * 0.1
-	
-    local worldYaw = input.getNumber(4) -- pose (estimation? nope!!! physics sensor magic)
+
+	-- pose (estimation? nope!!! physics sensor magic)
+    local worldYaw = input.getNumber(4) -- +-0.5 corresponding to +-180 degrees
 	local worldX = input.getNumber(5)
 	local worldY = input.getNumber(6)
-	-- local worldRoll = input.getNumber(9)  -- all of these are unused for now, im just leaving them here so that they can be quickly usedvlater
+	-- local worldRoll = input.getNumber(9)  -- all of these are unused for now, im just leaving them here so that they can be quickly used later
 	-- local worldPitch = input.getnumber(10)
 	-- local rotSpeed = input.getNumber(11)
 	-- local linSpeed = input.getNumber(12)
 	
-	local reqX = input.getNumber(7) -- auton stuff
+	-- auton stuff
+	local reqX = input.getNumber(7)
 	local reqY = input.getNumber(8)
 	local moveConfirm = input.getBool(4)
 	local aimAtCoord = input.getBool(5)
 	
-    local resetGyro = input.getBool(1) -- misc inputs
+	-- misc inputs
+    local resetGyro = input.getBool(1) 
 	local softBrake = input.getBool(2)
 	local hardBrake = input.getBool(3)
 
-    if worldCentric == true then -- world/field centric control
+	-- world/field centric control
+    if worldCentric == true then 
 		xVel, yVel = orient(xDir, yDir, worldYaw, resetGyro)
     else 
         xVel = xDir
         yVel = yDir
     end
+	
+	-- rotate to aim at point
 	local brake = 0
-	if aimAtCoord == true then -- rotate to aim at point
+	if aimAtCoord == true then 
    	 rotation, brake = calculateAimRotation(worldX, worldY, worldYaw, reqX, reqY)
 	end
-
-	if moveToCoord == true then -- translate to point
-	    xVel, yVel = calculateTranslationToPoint(worldX, worldY, worldYaw, reqX, reqY, worldCentric)
+	
+	-- translate to point
+	if moveToCoord == true then
+	    xVel, yVel, brake = TRANS(worldX, worldY, worldYaw, reqX, reqY, worldCentric)
 	end
 
-    local A = xVel - (rotation*length)/2 -- define wheel vector locations
+	-- define wheel vector locations
+    local A = xVel - (rotation*length)/2 
     local B = xVel + (rotation*length)/2
     local C = yVel - (rotation*width)/2
     local D = yVel + (rotation*width)/2
 
-    local s1, a1 = calculateWheel(B, C) -- get angle, speed
+	-- get angle, speed
+    local s1, a1 = calculateWheel(B, C)
     local s2, a2 = calculateWheel(B, D)
     local s3, a3 = calculateWheel(A, D)
     local s4, a4 = calculateWheel(A, C)
-	
-    local maxSpeed = math.max(s1, s2, s3, s4) -- compensate if max speed > 1
+
+	-- compensate if max speed > 1
+    local maxSpeed = math.max(s1, s2, s3, s4) 
 	if maxSpeed > 1 then
         s1 = s1/maxSpeed
         s2 = s2/maxSpeed
@@ -60,7 +71,8 @@ function onTick()
         s4 = s4/maxSpeed
     end
 
-	if modeCoast == false then -- brake when input released if coast active
+	-- brake when input released if coast mode inactive
+	if modeCoast == false then 
 		if math.abs(xVel) <= 0.1 and math.abs(yVel) <= 0.1 and math.abs(input.getNumber(3)) <= 0.1 then
 			brake = 1
 		else 
@@ -68,44 +80,50 @@ function onTick()
 		end
 	end
 
+	-- brakes (including wheel X lockup)
 	if softBrake == true then -- wheel brake only
 		brake = 1
-	elseif hardBrake == true then -- wheel brake, plus all wheels inward (not chassis centre)
+	elseif hardBrake == true then -- wheel brake, plus all wheels inward (not to chassis centre)
 		brake = 1
 		a1 = 225 -- front right
 		a2 = 135 -- front left
 		a3 = 45 -- back left
 		a4 = 315 -- back right
 	end
-		
-    output.setNumber(1, s1) -- speed encode
+
+	-- speed encode
+    output.setNumber(1, s1) 
     output.setNumber(2, s2)
     output.setNumber(3, s3)
     output.setNumber(4, s4)
-	
-	output.setNumber(5, brake) -- brake encode
-	
-    output.setNumber(11, a1) -- angle encode
+
+	-- brake encode
+	output.setNumber(5, brake) 
+
+	-- angle encode
+    output.setNumber(11, a1) 
     output.setNumber(12, a2)
     output.setNumber(13, a3)
     output.setNumber(14, a4)
 end
 
-function calculateWheel(X, Y) -- wheel speed and angle
+-- wheel speed and angle
+function calculateWheel(X, Y) 
 	local speed = math.sqrt(X^2 + Y^2)
 	local angle = math.atan(X, Y)*180/math.pi
 	return speed, angle
 end
 
+-- field/robot orient, incl. gyro reset function
 local gyroOffset = 0
-function orient(xDir, yDir, worldYaw, resetGyro) -- field/robot orient, incl. gyro reset function
+function orient(xDir, yDir, worldYaw, resetGyro)
     if resetGyro then
         gyroOffset = worldYaw
     end
-    local zeroedYaw = worldYaw - gyroOffset
+    local zeroedYaw = worldYaw - gyroOffset -- +-0.5 corresponding to +-180
     if zeroedYaw > 0.5 then zeroedYaw = zeroedYaw - 1 end
     if zeroedYaw < -0.5 then zeroedYaw = zeroedYaw + 1 end
-    local yaw_rad = -zeroedYaw * 2 * math.pi  -- negative here
+    local yaw_rad = -zeroedYaw * 2 * math.pi  -- radians
     local xVel = xDir * math.cos(yaw_rad) - yDir * math.sin(yaw_rad)
     local yVel = xDir * math.sin(yaw_rad) + yDir * math.cos(yaw_rad)
     return xVel, yVel
@@ -113,7 +131,7 @@ function orient(xDir, yDir, worldYaw, resetGyro) -- field/robot orient, incl. gy
 end
 
 function calcAim(worldX, worldY, worldYaw, reqX, reqY) -- move front of chassis to point at req x, y
-    local currentYaw = worldYaw * math.pi
+    local currentYaw = worldYaw * math.pi -- radians
     local deltaX = reqX - worldX
     local deltaY = reqY - worldY
     local targetAngle = math.atan2(deltaY, deltaX)
@@ -134,7 +152,8 @@ function calcAim(worldX, worldY, worldYaw, reqX, reqY) -- move front of chassis 
     return rotation, brake
 end
 
-function TRANS(worldX, worldY, worldYaw, reqX, reqY, worldCentric) -- translation :3 (the roy consumes)
+-- translation :3 (the roy consumes)
+function TRANS(worldX, worldY, worldYaw, reqX, reqY, worldCentric)
     local deltaX = reqX - worldX -- needed translation amount
     local deltaY = reqY - worldY
 	
@@ -147,8 +166,8 @@ function TRANS(worldX, worldY, worldYaw, reqX, reqY, worldCentric) -- translatio
     end
 
     local xVel, yVel
-
-    if worldCentric then -- correct movement if worldcentric
+	-- correct movement if worldcentric
+    if worldCentric then 
         local yaw = worldYaw * math.pi
         local cosYaw = math.cos(-yaw)
         local sinYaw = math.sin(-yaw)
@@ -158,14 +177,16 @@ function TRANS(worldX, worldY, worldYaw, reqX, reqY, worldCentric) -- translatio
         xVel = deltaX
         yVel = deltaY
     end
-	
-	local mag = math.sqrt(xVel^2 + yVel^2) -- compensate if vector magnitude > 1
+
+	-- compensate if vector magnitude > 1
+	local mag = math.sqrt(xVel^2 + yVel^2) 
 	if mag > 1 then
  	   xVel = xVel / mag
  	   yVel = yVel / mag
 	end
 
-    if distance < 0.2 then -- stop within x meters of target
+	-- stop within x meters of target
+    if distance < 0.2 then 
         xVel = 0
         yVel = 0
 		brake = 1
@@ -173,4 +194,5 @@ function TRANS(worldX, worldY, worldYaw, reqX, reqY, worldCentric) -- translatio
 	
     return xVel, yVel, brake
 end
+
 
