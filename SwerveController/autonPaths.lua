3eff3ddcd5396288db71e90a0513c9b275e373f5
Edsch9086 <edschaulat@gmail.com>
2025-10-31 10:59:48 -0700

    assign wp list and prep for actual logic

--- SwerveController/autonPaths.lua
+++ SwerveController/autonPaths.lua
@@ -3,5 +3,21 @@ function onTick()
     local wpAchv = input.getBool(1)
 
     -- wp coord list
-    local x1, y1 = input.getBool(1, 2)
+    local x1 = getBool(1)
+    local y1 = getBool(2)
+    local x2 = getBool(3)
+    local y2 = getBool(4)
+    local x3 = getBool(5)
+    local y3 = getBool(6)
+    local x4 = getBool(7)
+    local y4 = getBool(8)
+    local x5 = getBool(9)
+    local x5 = getBool(10)
+
+    -- wp target
+    local targX = 0
+    local targY = 0
+
+    output.setNumber(1, targX)
+    output.setNumber(2, targY)
 end
